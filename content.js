1462c2c48ffdde591a8fb97144fd478461739c38
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-05-09 01:17:22 +0530

    Fixed Bugs.

--- content.js
+++ content.js
@@ -1,21 +1,47 @@
-console.log("Hi You!!");
+console.log("HEYYYYYY!!");
 chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendResponse) {
   if (message.txt == "Hello") {
     const elements = document.getElementsByTagName("*");
-    for (let i = 0; i < elements.length; i++) {
-      var element = elements[i];
-      for (let j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
+ document.body.innerHTML = document.body.innerHTML.replace(
+   /npm init/gi,
+   "yarn init"
+ );
+  document.body.innerHTML = document.body.innerHTML.replace(
+    /npm run/gi,
+    "yarn run"
+  );
+ document.body.innerHTML = document.body.innerHTML.replace(
+   /npm link/gi,
+   "yarn link"
+ );
+ document.body.innerHTML = document.body.innerHTML.replace(
+   /npm publish/gi,
+   "yarn publish"
+ );
+ document.body.innerHTML = document.body.innerHTML.replace(
+   /npm install (.*) --global/gi,
+   "yarn global add"
+ );
+ document.body.innerHTML = document.body.innerHTML.replace(
+   /npm uninstall /gi,
+   "yarn remove"
+ );
+document.body.innerHTML = document.body.innerHTML.replace(
+  /npm install (.*) --save /gi,
+  /yarn add /
+);
 
-        if (node.nodeType === 3) {
-          var text = node.nodeValue;
-          var replacedText = text.replace(/npm (install|i)/gi, "yarn add");
-          if (replacedText !== text) {
-            element.replaceChild(document.createTextNode(replacedText), node);
-          }
-        }
-      }
-    }
+  document.body.innerHTML = document.body.innerHTML.replace(
+    /npm (install|i)/gi,
+    "yarn add"
+  );
+
+
+
+
+  
+ 
+
   }
 }
